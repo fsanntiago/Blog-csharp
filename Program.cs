5cc29a86e6d4bd0df810b4d855811580c8de5086
fsanntiago <zerofabricio@hotmail.com>
2022-10-05 10:52:48 -0300

    feat: CRUD User

--- Program.cs
+++ Program.cs
@@ -10,7 +10,11 @@ namespace Blog
 
         static void Main(string[] args)
         {
-            ReadUsers();
+            //ReadUsers();
+            //ReadUser();
+            //CreateUser();
+            //UpdateUser();
+            DeleteUser();
         }
 
         public static void ReadUsers()
@@ -22,6 +26,91 @@ namespace Blog
                 foreach (var user in users)
                 {
                     Console.WriteLine(user.Name);
+                }
+
+            }
+        }
+
+        public static void ReadUser()
+        {
+            using (var connection = new SqlConnection(CONNECTION_STRING))
+            {
+                var user = connection.Get<User>(1);
+
+                Console.WriteLine(user.Name);
+            }
+        }
+
+        public static void CreateUser()
+        {
+            var user = new User()
+            {
+                Bio = "Equipe balta.io",
+                Email = "hell@balta.io",
+                Image = "https://",
+                Name = "Equipe balta.io",
+                PasswordHash = "HASH",
+                Slug = "equipe-balta"
+            };
+
+            using (var connection = new SqlConnection(CONNECTION_STRING))
+            {
+                var rows = connection.Insert<User>(user);
+                if (rows == 0)
+                {
+                    Console.WriteLine("Cadastro falhou");
+                }
+                else
+                {
+                    Console.WriteLine("Cadastro realizado com sucesso");
+                }
+
+            }
+        }
+
+        public static void UpdateUser()
+        {
+            var user = new User()
+            {
+                Id = 2,
+                Bio = "Equipe | balta.io",
+                Email = "hell@balta.io",
+                Image = "https://...",
+                Name = "Equipe de suporte balta.io",
+                PasswordHash = "HASH",
+                Slug = "equipe-balta"
+            };
+
+            using (var connection = new SqlConnection(CONNECTION_STRING))
+            {
+                var res = connection.Update<User>(user);
+                if (!res)
+                {
+                    Console.WriteLine("Atualizacao falhou");
+                }
+                else
+                {
+                    Console.WriteLine("Atualizacao realizada com sucesso");
+                }
+
+            }
+        }
+
+        public static void DeleteUser()
+        {
+
+            using (var connection = new SqlConnection(CONNECTION_STRING))
+            {
+                var user = connection.Get<User>(2);
+
+                var res = connection.Delete<User>(user);
+                if (!res)
+                {
+                    Console.WriteLine("Exclusao falhou");
+                }
+                else
+                {
+                    Console.WriteLine("Exclusao realizada com sucesso");
                 }
 
             }
